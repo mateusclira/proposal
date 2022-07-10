7761c629a6f5cf9db2eafe861762faba44e8fa40
Mateus Lira <mateusc.lira@gmail.com>
2022-07-10 17:22:57 -0300

    changes on visual

--- docker/app/aplicacao.py
+++ docker/app/aplicacao.py
@@ -33,8 +33,27 @@ async def health():
 @app.get("/belzinha")
 async def belzinha():
     curiosidades = [
-        "Belzinha gosta muito de dormir e dorme em qualquer lugar",
-        "Belzinha gosta muito de azul! Eu também"
+        "Roberta gosta muito de dormir e dorme em qualquer lugar. Eu tenho dificuldade pra pegar no sono",
+        "Nós gostamos muito de azul!",
+        "Roberta tem uma agendinha azul",
+        "Mateus já deu um buquê de rosas pintadas de Azul para Roberta",
+        "Uma vez Roberta ganhou uma camisa azul enorme de um congresso, se deu o apelido de balão azul",
+        "Nosso primeiro encontro foi no cinema!",
+        "Cantor favorito em comum, Ed Sheeran. Inclusive, ouçam The Joker and the Queen.",
+        "Amamos Café! Eu sempre uso ela de cobaia perguntando se o café ainda está quente demais :3",
+        "Ela é a dentista mais pezinho pra dentro de TI que eu já vi na vida. Um dia ainda puxo ela",
+        "Houve um tempo no relacionamento que eu contava historinhas completas pra botar ela pra dormir.",
+        "Nós já passamos altas tardes lado a lado jogando league of legends",
+        "Todo ano trocamos livros de presentes",
+        "Ela me pediu em namoro na praia em Maceió",
+        "Estivemos desde sempre muito conectados. Escrevi um soneto pra ela 3 semanas atrás, ontem, ela me 'citou' 1 estrofe dele. É autoral.",
+        "Zeramos Resident Evil 7 em 1 dia. Começamos de manhã cedo e ficamos até de madrugada kkkk",
+        "Eu já sabia que ia casar com ela há cerca de 5 anos atrás, e mesmo assim a certeza parece que só aumenta a cada dia.",
+        "Começamos a namorar no dia 18/07/2016",
+        "Na época da faculdade vínhamos pra casa e ela deitava e literalmente dormia",
+        "Sempre que pensamos em ir pra Maceió acontece alguma coisa interessante na nossa vida. Alguém engravida, alguém casa, etc.",
+        "Ela entende muito de investimento!",
+        "Esse ano ela fez meu Imposto de Renda, me rendeu um bom lucro, que já está voltando para ela rs",
     ]
     return {
         "statusCode":200,
